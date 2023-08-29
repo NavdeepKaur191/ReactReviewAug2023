6de3de4093b57218ce14a3fd1e081974ca109d0a
Navdeep Kaur <navdeepkaur181986@gmail.com>
2023-08-29 10:11:36 -0600

    Complete upto UseEffect Hook

---
+++ React-contactList00/contactList/src/components/App.jsx
@@ -0,0 +1,15 @@
+import Contact from "./components/Contact.jsx";
+
+function App() {
+  return (
+    <div>
+      <Contact 
+      	name="Leroy Brown" 
+      	telNumber="(123)456-7890"   
+      	email="LB35@yahoo.com" 
+      />
+    </div>
+  );
+}
+
+export default App;
